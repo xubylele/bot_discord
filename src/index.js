7d44183d01290bce69a6c9650cd467a8e7a54459
Xavier Diaz <diazxavier27gmail.com>
2020-06-28 23:24:29 -0400

    TATA

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 const Discord = require('discord.js')
-const { prefix, token } = require('../config.json')
-const ytdl = require('ytdl-core')
+const { token } = require('../config.json')
 
 const bot = new Discord.Client()
-
-const queue = new Map()
 
 bot.once('ready', () => {
     console.log('Ready!')
@@ -17,113 +14,34 @@ bot.once('disconnect', () => {
 })
 
 
-bot.on('message', async message => {
-    if(message.author.bot) return
-
-    if(!message.content.startsWith(prefix)) return
-    const serveQueue = queue.get(message.guild.id)
-
-    if(message.content.startsWith(`${prefix}play`)){
-        execute(message, serveQueue)
-        return
-    }else if(message.content.startsWith(`${prefix}skip`)){
-        skip(message, serveQueue)
-        return
-    }else if(message.content.startsWith(`${prefix}stop`)){
-        stop(message, serveQueue)
-        return
-    }else{
-        message.channel.send('Ingresa un comando valido tonto weon')
-    }
-})
-
-async function execute(message, serverQueue) {
-    const args = message.content.split(' ')
-    const voiceChannel = message.member.voice.channel
+bot.on('messageReactionAdd', (reaction, user) => {
+    console.log(user)
+    console.log(reaction.message.channel.id)
+    if(user.username === 'Korbel'){
+        console.log(user.username)
+        if (reaction.emoji.id === "595739180999639050" || reaction.emoji.name === '🤡') {
+            let channel = bot.channels.cache.get(reaction.message.channel.id)
+            console.log(channel)
+            if(channel == null) return
+            const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('GRACIAS TATA')
+                .setDescription('GRACIAS TATA')
+                .setThumbnail('https://imgur.com/KLNZQXO.png')
+                .addFields(
+                    { name: 'GRACIAS TATA', value: 'GRACIAS TATA' },
+                    { name: '\u200B', value: '\u200B' },
+                    { name: 'GRACIAS TATA', value: 'GRACIAS TATA', inline: true },
+                    { name: 'GRACIAS TATA', value: 'GRACIAS TATA', inline: true },
+                )
+                .addField('GRACIAS TATA', 'GRACIAS TATA', true)
+                .setImage('https://imgur.com/KLNZQXO.png')
+                .setTimestamp()
+                .setFooter('GRACIAS TATA', 'https://imgur.com/KLNZQXO.png');
     
-    if(!voiceChannel) return message.channel.send('Entra a un canal de voz primero, tonto')
-    
-    const permissions = voiceChannel.permissionsFor(message.client.user)
-
-    if(!permissions.has('CONNECT') || !permissions.has('SPEAK')) return message.channel.send('No tengo permisos en ese canal de voz')
-
-    const songInfo = await ytdl.getInfo(args[1])
-    const song = {
-        title: songInfo.title,
-        url: songInfo.video_url
-    }
-
-    if(!serverQueue){
-
-        const queueContruct = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true
+            channel.send(exampleEmbed);
         }
-
-        queue.set(message.guild.id, queueContruct)
-        queueContruct.songs.push(song)
-
-        try {
-            var connection = await voiceChannel.join()
-            queueContruct.connection = connection
-            play(message.guild, queueContruct.songs[0])
-
-        } catch (error) {
-            console.log(error)
-            queue.delete(message.guild.id)
-            return message.channel.send(err)
-        }
-    }else{
-        serverQueue.songs.push(song)
-        console.log(serverQueue.songs)
-        const embed = {
-            color: ('#460101'),
-            title: ('Bot Queue'),
-            url: (song.url),
-            fields: [
-                { name: 'Cancion', value: song.title },
-                { name: 'Status', value: 'agregado con exito' }
-            ]
-        }
-        return message.channel.send({embed: embed})
     }
-    
-}
-
-function play(guild, song) {
-    const serverQueue = queue.get(guild.id)
-    if(!song){
-        serverQueue.voiceChannel.leave()
-        queue.delete(guild.id)
-        return
-    }
-
-    const dispatcher = serverQueue.connection.play(ytdl(song.url))
-
-    
-
-    dispatcher.on('end', () => {
-        console.log('Music ended!')
-
-        serverQueue.song.shift()
-
-        play(guild, serverQueue.songs[0])
-    }).on('error', error => {
-        console.log(error)
-    })
-
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5)
-}
-
-function stop(message, serverQueue) {
-    if(!message.member.voice.channel) return message.channel.send('Entra a un canal de voz primero, tonto')
-
-    serverQueue.songs = []
-    serverQueue.connection.dispatcher.end()
-}
+});
 
 bot.login(token)
